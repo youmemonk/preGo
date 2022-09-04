e7aeaf3eb8cdfa844b0b5e24b1921503722e3a9b
Mayank Ukey <mayank.ukey@searce.com>
2022-09-04 23:19:17 +0530

    Structs Intro

--- structs/main.go
+++ structs/main.go
@@ -2,17 +2,42 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
 	// alex := person{firstName: "Alex", lastName: "Anderson"}
-	var alex person
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
 
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Halpert",
+		contactInfo: contactInfo{
+			email:   "hijim@gmail.com",
+			zipCode: 1245,
+		},
+	}
+
+	jim.updateName("Jimmy")
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
 }
